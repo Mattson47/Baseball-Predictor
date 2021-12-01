3887bc426e73a62d82d630de24b353ab0871167a
Andrew Mattson <a47mattson@att.net>
2021-12-01 18:00:57 -0500

    Initial Commit

---
+++ project.py
@@ -0,0 +1,18 @@
+# project.py
+# a program to predict a baseball players future performance and how much they should be paid
+
+def main():
+    pass
+    # get data from an online source
+        #import pybaseball
+    
+    # create a dictonary for every player from the data set using their batting statistics
+
+    # ask user for a players name to pull up the statistics
+
+    # predict how much a player should make
+
+
+
+if __name__ == "__main__":
+    main()
