a3ac040040777b3adbc49d7885c08b1588bb8b15
Andrew Mattson <a47mattson@att.net>
2021-12-06 17:39:38 -0500

    Trying to use the data

--- project.py
+++ project.py
@@ -1,13 +1,16 @@
 # project.py
 # a program to predict a baseball players future performance and how much they should be paid
+import numpy as np
+import pandas as pd
+from pybaseball import batting_stats 
 
 def main():
-    pass
-    # get data from an online source
-        #import pybaseball
+    # get the data out of pybaseball
+    data = batting_stats(2010,2021)
+    RBI = data('Name','Age','Rbi')
+    outfile = open("RBI.txt")
+    print(RBI)
     
-    # create a dictonary for every player from the data set using their batting statistics
-
     # ask user for a players name to pull up the statistics
 
     # predict how much a player should make
