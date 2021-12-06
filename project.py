a8512561cb6e42d8f694c315359bd0d2164d3f75
Matthew Eicholtz <meicholtz@flsouthern.edu>
2021-12-06 17:56:31 -0500

    extracted relevant stats from pandas data frame

--- project.py
+++ project.py
@@ -3,13 +3,23 @@
 import numpy as np
 import pandas as pd
 from pybaseball import batting_stats 
+import pdb
+
 
 def main():
     # get the data out of pybaseball
-    data = batting_stats(2010,2021)
-    RBI = data('Name','Age','Rbi')
-    outfile = open("RBI.txt")
-    print(RBI)
+    data = batting_stats(2020,2021)
+    x = np.array(data)
+    columns = data.columns.tolist()
+    col = ['Name', 'Age', 'RBI']
+
+    stats = []
+    for name in col:
+        ind = columns.index(name)
+        stats.append(x[:, ind])
+    stats = np.array(stats).transpose()
+
+    pdb.set_trace()
     
     # ask user for a players name to pull up the statistics
 
